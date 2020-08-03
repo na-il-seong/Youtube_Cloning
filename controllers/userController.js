b8207e5d30f61db845a3cc21839e54436b8ea008
na-il-seong <68512686+na-il-seong@users.noreply.github.com>
2020-08-04 00:32:48 +0900

    Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,26 +30,35 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) =>
+export const getLogin = (req, res) =>
   res.render("login", {
     pageTitle: "Login",
   });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+}
+
 export const logout = (req, res) =>
   res.render("logout", {
     pageTitle: "Logout",
   });
+
 export const users = (req, res) =>
   res.render("users", {
     pageTitle: "Users",
   });
+
 export const userDetail = (req, res) =>
   res.render("userDetail", {
     pageTitle: "User Detail",
   });
+
 export const editProfile = (req, res) =>
   res.render("editProfile", {
     pageTitle: "Edit Profile",
   });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", {
     pageTitle: "Change Password",
