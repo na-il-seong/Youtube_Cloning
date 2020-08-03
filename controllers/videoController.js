0544ca9e8ef7bb46c9df614cbbb35628a1a3724f
na-il-seong <68512686+na-il-seong@users.noreply.github.com>
2020-08-03 22:13:18 +0900

    Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,33 +1,38 @@
-export const home = (req, res) =>
-  res.render("home", {
-    pageTitle: "Home",
-  });
-export const search = (req, res) => {
-  const {
-    query: { term: searchingBy },
-  } = req;
-  res.render("search", {
-    pageTitle: "Search",
-    searchingBy,
-  });
+import {
+    videos
+} from "../db"
+
+export const home = (req, res) => {
+    res.render("home", {
+        pageTitle: "Home",
+        videos
+    });
 };
-export const videos = (req, res) =>
-  res.render("videos", {
-    pageTitle: "Videos",
-  });
+export const search = (req, res) => {
+    const {
+        query: {
+            term: searchingBy
+        },
+    } = req;
+    res.render("search", {
+        pageTitle: "Search",
+        searchingBy,
+    });
+};
+
 export const upload = (req, res) =>
-  res.render("upload", {
-    pageTitle: "Upload",
-  });
+    res.render("upload", {
+        pageTitle: "Upload"
+    });
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", {
-    pageTitle: "Video Detail",
-  });
+    res.render("videoDetail", {
+        pageTitle: "Video Detail"
+    });
 export const editVideo = (req, res) =>
-  res.render("editVideo", {
-    pageTitle: "Edit Video",
-  });
+    res.render("editVideo", {
+        pageTitle: "Edit Video"
+    });
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", {
-    pageTitle: "Delete Video",
-  });
+    res.render("deleteVideo", {
+        pageTitle: "Delete Video"
+    });
