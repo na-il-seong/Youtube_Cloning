8457d18fb51766e85cc033ce81bfe602f2c021a9
na-il-seong <68512686+na-il-seong@users.noreply.github.com>
2020-08-06 00:08:38 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,45 +1,15 @@
-export const videos = [{
-    id: 323541,
-    title: "Awesome Video",
-    description: "This is something awesome",
-    views: 23,
-    videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
-    creator: {
-        id: 434322,
-        name: "ilseong",
-        email: "nis@gmail.com"
-    }
-}, {
-    id: 323542,
-    title: "Sexy Video",
-    description: "This is something sexy",
-    views: 254,
-    videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
-    creator: {
-        id: 434322,
-        name: "ilseong",
-        email: "nis@gmail.com"
-    }
-}, {
-    id: 323543,
-    title: "Super Video",
-    description: "This is something super",
-    views: 476,
-    videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
-    creator: {
-        id: 434322,
-        name: "ilseong",
-        email: "nis@gmail.com"
-    }
-}, {
-    id: 323544,
-    title: "Cool Video",
-    description: "This is something cool",
-    views: 1039,
-    videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
-    creator: {
-        id: 434322,
-        name: "ilseong",
-        email: "nis@gmail.com"
-    }
-}];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/Wetube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ Connected to DB");
+
+const handleError = (error) => console.log(`Error: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
