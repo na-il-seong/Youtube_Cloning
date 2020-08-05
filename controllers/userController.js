24cd9b0e969b6238e01edb1111a4ae192c689ebd
na-il-seong <68512686+na-il-seong@users.noreply.github.com>
2020-08-06 03:29:31 +0900

    Home Controller Finish

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,7 @@
 import routes from "../routes";
 
 export const getJoin = (req, res) => {
-  res.render("join", {
-    pageTitle: "Join",
-  });
+  res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
@@ -12,9 +10,7 @@ export const postJoin = (req, res) => {
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render("join", {
-      pageTitle: "Join",
-    });
+    res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
     // To Do: Log User In
@@ -23,9 +19,7 @@ export const postJoin = (req, res) => {
 };
 
 export const getLogin = (req, res) =>
-  res.render("login", {
-    pageTitle: "Login",
-  });
+  res.render("login", { pageTitle: "Login" });
 
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
@@ -36,22 +30,13 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const users = (req, res) =>
-  res.render("users", {
-    pageTitle: "Users",
-  });
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 
 export const userDetail = (req, res) =>
-  res.render("userDetail", {
-    pageTitle: "User Detail",
-  });
+  res.render("userDetail", { pageTitle: "User Detail" });
 
 export const editProfile = (req, res) =>
-  res.render("editProfile", {
-    pageTitle: "Edit Profile",
-  });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 
 export const changePassword = (req, res) =>
-  res.render("changePassword", {
-    pageTitle: "Change Password",
-  });
+  res.render("changePassword", { pageTitle: "Change Password" });
